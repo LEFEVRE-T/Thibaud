b29428012f9463398592a45993668ed27186ef9d
LEFEVRE-T <thibaud.lefevre@etu.parisdescartes.fr>
2020-03-24 14:09:03 +0000

    complements

--- python.py
+++ python.py
@@ -1,15 +1,18 @@
-# PARTIE CODE
+# PARTIE IMPORTS
 
 import pandas as pd
 import matplotlib.pyplot as plt
 import statistics as st
 import random as rd
+import collections as cl
+
+
+# PARTIE CODE
 
 d = {'X' : pd.Series([1, 2, 3, 4], index=['a', 'b', 'c', 'd']),
     'Y' : [1, 2, 3, 4],
     'Z' : ['A', 'B', 'C', 'D']}
 df = pd.DataFrame(d)
-
 
 def choix_donnees():
 
@@ -28,38 +31,51 @@ def while_liste(txt, liste, type = ''):
   return(variable)
 
 
-def indicateurs(nom, type):
-  if type == quanti:
-    mini = min(nom)
-   maxi = max(nom)
-   mean = st.mean(nom)
-   median = st.median(nom)
-   sd = st.stdev(nom)
-   var = st.variance(nom)
-  
-  else:
-    print("l")
+def compter(data):
+  dic = cl.defaultdict(int)
+  for element in data:
+    dic[element] += 1
+  print(dic.items())
 
-  return(pd.DataFrame([mini, maxi, mean, median, sd, var], index = ['Min', 'Max', 'Mean', 'Med', 'Sd', 'Var']))
+
+def indicateurs(nom, type):
+
+  if type == "quanti":
+    mini = min(nom)
+    maxi = max(nom)
+    mean = st.mean(nom)
+    median = st.median(nom)
+    sd = st.stdev(nom)
+    var = st.variance(nom)  
+    print(pd.DataFrame([mini, maxi, mean, median, sd, var], index = ['Min', 'Max', 'Mean', 'Med', 'Sd', 'Var']))
+  else:
+    compter(nom)
+    
+
+def realisation(data, choix):
+
+  choix = int(choix)
+  if choix == 1:
+    print(df)
+  elif choix == 2:
+    colonne = while_liste('Nom de colonne sur lequel faire : ', ['X', 'Y', 'Z'], str)
+    type = while_liste('Type de données entre "quali" & "quanti" : ', ['quali', 'quanti'], str)
+    indicateurs(df[colonne], type)
+  else:
+    print("r")
 
 
 def choix():
-  print(" 1 : Afficher données \n 2 : Calculer indicateurs \n 3 : Autres \n Entrez le numéro du choix")
-  action = -1
-  while action != 3:
-    action = while_liste('Que voulez-vous faire : ', range(1, 4), int)
-    if action == 1:
-      print(df)
-    elif action == 2:
-      colonne = while_liste('Nom de colonne sur lequel faire : ', ['X', 'Y'], str)
-      type = while_liste('Type de données entre "quali" & "quanti" : ', ['quali', 'quanti'], str)
-      print(indicateurs(df[colonne], type))
-    else:
-      print("r")
+
+  while True:
+    print('\n 1 : Afficher données \n 2 : Calculer indicateurs \n 3 : Autres \n Entrez le numéro du choix \n "exit" pour sortir')
+    action = while_liste('Que voulez-vous faire : ', ["1", "2", "3", "exit"], str)
+    if action == "exit":
+      break
+    realisation(df, action)
 
 
 # PARTIE UTILISATEUR
-
 
 donnees = choix_donnees()
 choix()
